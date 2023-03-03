8f66efb4aa62621ac13a899aac74782e9775d550
Zac Walters <zacwalters4@gmail.com>
2023-03-03 16:55:01 -0700

    Complete file structure

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Routes, Route } from "react-router-dom"
+import Header from '../Header/Header'
+import Home from '../Home/Home'
+import Account from '../Account/Account'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/account" element={<Account />}>
+          <Route index element={<Account />} />
+          <Route path=":accountName" element={<Account />} />
+        </Route>
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+
+    return (
+        <main className="Home">
+            <h1>hi</h1>
+        </main>
+    )
+  }
+  
+  export default Home

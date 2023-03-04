e661828cf36ce74900c9487f03d81f9ec5875a16
Zac Walters <zacwalters4@gmail.com>
2023-03-03 21:31:35 -0700

    Add api call and set state to data

--- src/Account/Account.jsx
+++ src/Account/Account.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import './Account.css'
+import { useLocation } from "react-router-dom"
+import { formatAccountName } from '../Utilities/Helper'
+import { fetchAccount } from '../Utilities/APICalls'
 
 function Account() {
+    const locationData = useLocation()
+    const accountName = formatAccountName(locationData.pathname)
+    const [accountData, setAccountData] = React.useState([])
+
+    const getAccount = () => {
+        fetchAccount(accountName)
+        .then(data => {
+            setAccountData(data)
+        })
+    }
+
+    React.useEffect(() => {
+        getAccount()
+      }, [])
 
     return (
         <main className="Account">
-            <h1>hi</h1>
+            <div>
+                <h1>{accountName}</h1>
+            </div>
         </main>
     )
   }
--- src/Utilities/Helper.jsx
+++ src/Utilities/Helper.jsx
@@ -1,3 +1,7 @@
 export const formatURL = (accountName) => {
     return accountName.replace('#', '-')
 }
+
+export const formatAccountName = (accountURL) => {
+    return accountURL.replace('/account/', '')
+}

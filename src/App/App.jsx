32f517702eda6375ff00b77b9c16f229f3935b73
Zac Walters <zacwalters4@gmail.com>
2023-03-03 18:03:10 -0700

    Add 404 handling

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import Header from '../Header/Header'
 import Home from '../Home/Home'
 import Account from '../Account/Account'
+import Error404 from '../Error404/Error404'
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
           <Route index element={<Account />} />
           <Route path=":accountName" element={<Account />} />
         </Route>
+        <Route path="/404" element={<Error404 />} />
+        <Route path="*" element={<Navigate to="/404" replace />}
+    />
       </Routes>
     </div>
   )

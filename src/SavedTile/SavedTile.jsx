4e90b1504f37ccdf22d323aa99b3874a59475aec
Zac Walters <zacwalters4@gmail.com>
2023-03-05 16:31:46 -0700

    Add link to saved page

--- src/SavedTile/SavedTile.jsx
+++ src/SavedTile/SavedTile.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { formatBattleTag } from '../Utilities/Helper'
 import './SavedTile.css'
+import { Link } from "react-router-dom"
 
 function SavedTile(props) {
     const deleteSaved = (e) => {
@@ -12,7 +13,9 @@ function SavedTile(props) {
 
     return (
         <div className="saved-tile">
+            <Link className="account-name" to={`/account/${props.account.accountName}`}>
             <p>{formatBattleTag(props.account.accountName)}</p>
+            </Link>
             <p>{props.account.accountScore}</p>
             <button onClick={deleteSaved}>Remove</button>
         </div>
